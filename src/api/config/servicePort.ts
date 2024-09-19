a2c505045f982c6a0093c8cdadbe74cbeebd1496
cloud-mouse <>
2024-09-19 19:46:12 +0800

    fix: 修改mock问题

--- src/api/config/servicePort.ts
+++ src/api/config/servicePort.ts
@@ -1,8 +1,8 @@
 // * 后端接口请求地址1服务
 const BASEURL1: any = {
   dev: '/mock',
-  test: 'http://127.0.0.1:8080/mock',
-  sit: '',
-  prod: '',
+  test: '/mock',
+  sit: '/mock',
+  prod: '/mock',
 }
 export const PORT1 = BASEURL1[import.meta.env.VITE_APP_MODE]
